5af00d68b05a598c5cad97f39ae1ec95997a7089
quy nguyen <chjcovay@gmail.com>
2019-10-24 11:03:15 +0700

    state

--- src/App.js
+++ src/App.js
@@ -1,31 +1,65 @@
-import React,{Component} from 'react';
-// import logo from './logo.svg';
+import React, {Component} from 'react';
+
 import './App.css';
-import SanPham from './components/SanPham/SanPham';
-// import SinhVien from './components/SinhVien/SinhVien';
+import SinhVien from './components/SinhVien/SinhVien';
+
+
 class App extends Component{
-  render() {
-    return(
-      <div className="App" >
-        <header className="App-header">
-      <h1>Ứng dụng React đầu tiên</h1>
-        <p>Đến từ nguyễn Văn Quý</p>
- <SanPham maSanPham='001'  tenSanPham='Huwai' gia='10000000VNĐ' />
-   Máy đắc và mau hư
-   <SanPham maSanPham='001' tenSanPham='Oppo' gia='100000000VNĐ' />
-   Máy chụp ảnh đẹp 
-   <SanPham maSanPham='001' tenSanPham='Iphone' gia='120000000VNĐ' />
-   Máy lướt facebook tốt
-   <SanPham maSanPham='001' tenSanPham='Samsung' gia='150000000VNĐ' />
-   Máy chơi game k đc
-   <SanPham maSanPham='001' tenSanPham='Nokia' gia='11000000VNĐ' />
-   Đôi chó chết tươi
-   </header>
-      </div>
 
-      
+  state = {
+    sinhVien : [
+      { hoten: 'Lionel Messi', tuoi: 35, sothich: 'Ăn nhậu' },
+      { hoten: 'RonalDo', tuoi: 32, sothich: 'Code' },
+      { hoten: 'Neymar JR', tuoi: 30, sothich: 'Giả vờ ngã' }
+    ],
+    lop : '18CDTH11'
+  }
+
+  changeClick = () => {
+    this.setState(
+      {
+        sinhVien : [
+          { hoten: 'Salah', tuoi: 5, sothich: 'Đá bóng' },
+          { hoten: 'Bale', tuoi: 2, sothich: 'Đá cầu' },
+          { hoten: 'Countinho', tuoi: 10, sothich: 'Hát' }
+        ],
+        lop : '18CDKT11'
+      }
     );
-  }   
+  }
 
-};
+  render(){
+    return(
+      <div className="App">
+        <header className="App-header">
+        <h2>ReactJS - {this.state.lop}</h2>
+        <button onClick={this.changeClick}>Change click</button>
+         <SinhVien
+           hoten = {this.state.sinhVien[0].hoten}
+           tuoi = {this.state.sinhVien[0].tuoi} >
+           Sở thích : {this.state.sinhVien[0].sothich}
+         </SinhVien>
+
+         <SinhVien
+           hoten = {this.state.sinhVien[1].hoten}
+           tuoi = {this.state.sinhVien[1].tuoi} >
+           Sở Thích : {this.state.sinhVien[1].sothich}
+         </SinhVien>
+
+         <SinhVien
+           hoten = {this.state.sinhVien[2].hoten}
+           tuoi = {this.state.sinhVien[2].tuoi} >
+           Sở Thích : {this.state.sinhVien[2].sothich}
+         </SinhVien>
+        </header>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
 export default App;
